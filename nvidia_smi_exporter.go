d2e87c3894f732fb4911d1b4e7d1e02096cb165e
phongnt <genycopedison@gmail.com>
2021-01-20 16:16:42 +0700

    Export nvidia-smi dmon

--- nvidia_smi_exporter.go
+++ nvidia_smi_exporter.go
@@ -12,13 +12,55 @@ import (
 )
 
 
+func dmon(response http.ResponseWriter, request *http.Request) {
+    out, err := exec.Command("nvidia-smi", "dmon", "-c", "1", "-s", "u").Output()
+
+    if err != nil {
+        fmt.Printf("%s\n", err)
+        return
+    }
+
+    reader := bufio.NewReader(bytes.NewReader(out))
+    line, isPrefix, err := reader.ReadLine()
+    line, isPrefix, err = reader.ReadLine()
+    line, isPrefix, err = reader.ReadLine()
+    var records [][]string
+    for err == nil && !isPrefix {
+        scanner := bufio.NewScanner(strings.NewReader(string(line)))
+        scanner.Split(bufio.ScanWords)
+        count := 0
+        var row []string
+        for scanner.Scan() && count < 5 {
+            row = append(row, scanner.Text())
+        }
+        records = append(records, row)
+        line, isPrefix, err = reader.ReadLine()
+    }
+
+    metricList := []string {
+        "utilization.sm", "utilization.mem", "utilization.enc", "utilization.dec"}
+
+    result := ""
+    for _, row := range records {
+        name := fmt.Sprintf("GPU[%s]", row[0])
+        for idx, value := range row[1:] {
+            result = fmt.Sprintf(
+                "%s%s{gpu=\"%s\"} %s\n", result,
+                metricList[idx], name, value)
+        }
+    }
+
+    fmt.Fprintf(response, strings.Replace(result, ".", "_", -1))
+}
+
 // name, index, temperature.gpu, utilization.gpu,
 // utilization.memory, memory.total, memory.free, memory.used
+// clocks.gr, clocks.video, clocks.sm
 
 func metrics(response http.ResponseWriter, request *http.Request) {
     out, err := exec.Command(
         "nvidia-smi",
-        "--query-gpu=name,index,temperature.gpu,utilization.gpu,utilization.memory,memory.total,memory.free,memory.used",
+        "--query-gpu=name,index,temperature.gpu,utilization.gpu,utilization.memory,memory.total,memory.free,memory.used,clocks.gr,clocks.video,clocks.sm",
         "--format=csv,noheader,nounits").Output()
 
     if err != nil {
@@ -59,6 +101,7 @@ func main() {
     }
 
     http.HandleFunc("/metrics/", metrics)
+    http.HandleFunc("/dmon/", dmon)
     err := http.ListenAndServe(addr, nil)
     if err != nil {
         log.Fatal("ListenAndServe: ", err)
